62e79e758686bfe52ae92ec4c60f064791f4ee70
Augusto <augustocolombelli@gmail.com>
2023-04-06 19:45:57 -0300

    Add log to book controller

--- src/main/java/com/books/api/BookController.java
+++ src/main/java/com/books/api/BookController.java
@@ -2,11 +2,16 @@ package com.books.api;
 
 import com.books.model.Book;
 import com.books.repository.BookRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class BookController {
 
@@ -18,6 +23,7 @@ public class BookController {
             @PathVariable String titleOrAuthorOrDescription,
             @RequestParam(defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "10") Integer size) {
+        log.info("Book requested for filter={}, page={}, size={}", titleOrAuthorOrDescription, page, size);
         return bookRepository.findByTitleOrAuthorOrDescription(titleOrAuthorOrDescription, PageRequest.of(page, size));
     }
 
